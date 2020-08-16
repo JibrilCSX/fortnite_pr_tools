3691a7c89599eb7c2b0963f12dcac123f128a290
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2020-08-16 16:31:21 -0500

    reformat code

--- cron_inactive_checks.py
+++ cron_inactive_checks.py
@@ -1,10 +1,12 @@
 from river_mwclient.esports_client import EsportsClient
 from river_mwclient.auth_credentials import AuthCredentials
 import mwparserfromhell, datetime
+
 limit = -1
 
-site = EsportsClient('fortnite', user_file="bot") # Set wiki
+site = EsportsClient('fortnite', user_file="bot")  # Set wiki
 summary = 'Automatically setting active/inactive status'
+
 
 def change_active_status(result, status):
     for p in result:
@@ -17,25 +19,26 @@ def change_active_status(result, status):
         if newtext != text:
             p.save(newtext, summary=summary)
 
+
 now = datetime.datetime.now()
-then = now - datetime.timedelta(days=6*28)
+then = now - datetime.timedelta(days=6 * 28)
 
 result = site.cargo_client.page_list(
-                  tables='Tournaments=T,TournamentResults=Res, TournamentResults__RosterLinks=RL,PlayerRedirects=PR,Players=P',
-                  join_on = 'T._pageName=Res.OverviewPage,Res._ID=RL._rowID,RL._value=PR.AllName,PR._pageName=P._pageName',
-                  where = 'P.IsInactive="1"',
-                  fields = 'P._pageName=player',
-                  having = 'COUNT(*)>1',
-                  )
+    tables='Tournaments=T,TournamentResults=Res, TournamentResults__RosterLinks=RL,PlayerRedirects=PR,Players=P',
+    join_on='T._pageName=Res.OverviewPage,Res._ID=RL._rowID,RL._value=PR.AllName,PR._pageName=P._pageName',
+    where='P.IsInactive="1"',
+    fields='P._pageName=player',
+    having='COUNT(*)>1',
+)
 
 change_active_status(result, 'No')
 
 result = site.cargo_client.page_list(
-                  tables='Tournaments=T,TournamentResults=Res, TournamentResults__RosterLinks=RL,PlayerRedirects=PR,Players=P',
-                  join_on = 'T._pageName=Res.OverviewPage,Res._ID=RL._rowID,RL._value=PR.AllName,PR._pageName=P._pageName',
-                  where = 'P.IsInactive IS NULL OR P.IsInactive="0"',
-                  fields = 'P._pageName=player',
-                  having = 'COUNT(*)=1 AND MAX(T.Date) <="{}"'.format(then.strftime('%Y-%m-%d')),
-                  )
+    tables='Tournaments=T,TournamentResults=Res, TournamentResults__RosterLinks=RL,PlayerRedirects=PR,Players=P',
+    join_on='T._pageName=Res.OverviewPage,Res._ID=RL._rowID,RL._value=PR.AllName,PR._pageName=P._pageName',
+    where='P.IsInactive IS NULL OR P.IsInactive="0"',
+    fields='P._pageName=player',
+    having='COUNT(*)=1 AND MAX(T.Date) <="{}"'.format(then.strftime('%Y-%m-%d')),
+)
 
 change_active_status(result, 'Yes')
