77d023e9341cc8b41c6ea599e98442552137b06d
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2020-09-17 07:04:39 -0500

    add fortnite_id to player page

--- cron_new_players.py
+++ cron_new_players.py
@@ -3,19 +3,32 @@ from river_mwclient.auth_credentials import AuthCredentials
 import mwparserfromhell, time
 
 limit = -1
+quiet = True
 
 credentials = AuthCredentials(user_file="me")
 site = EsportsClient('fortnite', credentials=credentials)  # set wiki
 summary = 'Automatically create player pages for Power Rankings'
 
+# RosterIds and RosterLinks should obviously be its own table
+# but the table schema sucks and I don't want to redesign the entire thing
+# so we'll get one row for each player that doesn't exist but then we'll still
+# have to look up in the full list of links and ids
+# to figure out which id this corresponds to
+# both RosterLinks and RosterIds are List-of-typed fields
+# but the proper schema for this would obviously be to have a separate table TournamentPlayers
+# and just have Player & ID and none of this
+# list-type fields were a mistake
+
 result = site.cargo_client.query(
     tables='TournamentResults=TR,TournamentResults__RosterLinks=RL,_pageData=PD,Tournaments=T',
     join_on='TR._ID=RL._rowID,RL._value=PD._pageName,TR.OverviewPage=T._pageName',
     where='PD._pageName IS NULL AND RL._value IS NOT NULL AND TR.PRPoints > "0"',
-    fields='RL._value=name,T.Region=res',
+    fields='RL._value=name,T.Region=res, TR.RosterLinks=RosterLinks, TR.RosterIds=RosterIds',
     group_by='RL._value',
     limit='max'
 )
+if not quiet:
+    print(result)
 default_text = site.client.pages['Help:Player Template'].text()
 default_text = default_text.replace('<noinclude>', '').replace('</noinclude>', '').strip()
 
@@ -33,20 +46,33 @@ for item in result:
         break
     lmt = lmt + 1
     name = item['name']
+    print(name)
+    print(item)
     res = item['res']
+    idx = None
+    if isinstance(item['RosterLinks'], str):
+        idx = item['RosterIds']
+    for i, namex in enumerate(item['RosterLinks']):
+        if namex == name:
+            idx = item['RosterIds'][i]
+            break
     if name == '0':
         continue
     try:
         page = site.client.pages[name]
         if page.text() != '':
-            # print('Page %s already exists, skipping' % name)
+            if not quiet:
+                print('Page %s already exists, skipping' % name)
             page.save(page.text())
             continue
-        # print('Processing page %s...' % name)
+        if not quiet:
+            print('Processing page %s...' % name)
         this_template.add('residency', res)
         this_template.add('id', name)
+        if idx is not None:
+            this_template.add('fortnite_id', idx)
         text = str(wikitext)
         page.save(text, summary=summary)
     except Exception as e:
         time.sleep(10)
-        site.client.pages['User:RheingoldRiver/auto players errors'].save(e)
+        site.client.pages['User:RheingoldRiver/auto players errors'].append('\n' + str(e) + ', player: ' + name)
